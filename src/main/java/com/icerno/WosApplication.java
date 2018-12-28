5b2d9e6efdea66e7df7886dcf7c5abf7c80b10e6
黄文柳 <huang.wenliu@icerno.com>
2018-12-28 09:25:38 +0800

    first commit

---
+++ src/main/java/com/icerno/WosApplication.java
@@ -0,0 +1,14 @@
+package com.icerno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WosApplication.class, args);
+    }
+
+}
+
